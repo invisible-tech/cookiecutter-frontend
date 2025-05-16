dff7a8dfeb5aa32bfdd8d0558c4ee748a2ba85e0
mateo-saldivar-inv <mateo.felgueroso@invisible.email>
2025-05-16 14:45:15 -0600

    Update pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python
+
 print("Testing Hook..............")
+
+tests_input = context["cookiecutter"]["tests"]
+code = tests_input.split(":")[0].strip().upper()
+context["cookiecutter"]["tests"] = code
+
+flags = {
+    "unit": code in ("U", "UI", "UE", "UIE"),
+    "integration": code in ("I", "UI", "IE", "UIE"),
+    "e2e": code in ("E", "UE", "IE", "UIE"),
+}
+
+context["cookiecutter"]["tests_flags"] = flags
+context["cookiecutter"].update(flags)
